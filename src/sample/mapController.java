a8e8a21428edd83c04019bcf8176c166f24c0dd9
Mustafa Pašić <62052767+mpasic2@users.noreply.github.com>
2021-01-05 23:54:56 +0100

    Initial commit

---
+++ src/sample/mapController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+    public class mapController implements Initializable {
+        public ImageView imgAbout;
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+
+            Image image = new Image("/img/logo.png");
+            imgAbout.setImage(image);
+
+        }
+    }
+
+
