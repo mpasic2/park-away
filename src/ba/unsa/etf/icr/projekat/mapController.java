ff4d0b07c001aaba9b1398aff4edac2132387a32
mDrinjak <mdriinjak1@etf.unsa.ba>
2021-01-12 13:17:32 +0100

    Implementiran side bar

--- src/ba/unsa/etf/icr/projekat/mapController.java
+++ src/ba/unsa/etf/icr/projekat/mapController.java
@@ -1,37 +1,69 @@
 package ba.unsa.etf.icr.projekat;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import com.jfoenix.transitions.hamburger.HamburgerNextArrowBasicTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 
 public class mapController implements Initializable {
         public ImageView imgAbout;
-        public Button dugmeMenuMap;
+
         public TextField fldSearch;
         private String user;
         public mapController(String user) {
             this.user = user;
         }
-
+        @FXML
+        private JFXDrawer drawer;
+        @FXML
+        private JFXHamburger dugmeMenuMap;
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
             Image image = new Image("/img/logo.png");
             imgAbout.setImage(image);
 
+            GridPane gp = null;
+            try {
+                gp = FXMLLoader.load(getClass().getResource("/fxml/mapFilter.fxml"));
+                drawer.setSidePane(gp);
+            } catch (IOException e) {
+                Logger.getLogger(mapController.class.getName()).log(Level.SEVERE,null,e);
+            }
+
+
+
+            dugmeMenuMap.addEventHandler(MouseEvent.MOUSE_CLICKED, e-> {
+                HamburgerNextArrowBasicTransition transition = new HamburgerNextArrowBasicTransition(dugmeMenuMap);
+                transition.setRate(transition.getRate() * -1);
+                transition.play();
+                if (drawer.isOpened())
+                    drawer.close();
+                else
+                    drawer.open();
+
+            });
         }
 
         public void logOut(ActionEvent actionEvent) throws IOException {
@@ -43,6 +75,7 @@ public class mapController implements Initializable {
             Stage close=(Stage)fldSearch.getScene().getWindow();
             close.close();
         }
+
     }
 
 
