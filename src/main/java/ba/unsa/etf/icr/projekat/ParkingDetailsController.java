71529ac595c0901291729a02e81949f5cc37213e
Mustafa Pasic <mpasic2@etf.unsa.ba>
2021-08-14 14:37:36 +0200

    dopunjena baza, dodan ekran te slike parkinga

---
+++ src/main/java/ba/unsa/etf/icr/projekat/ParkingDetailsController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.icr.projekat;
+
+import javafx.event.ActionEvent;
+
+public class ParkingDetailsController {
+
+
+    public void backParking(ActionEvent actionEvent) {
+    }
+
+}
