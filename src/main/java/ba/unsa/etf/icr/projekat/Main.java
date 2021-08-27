70c63bfc4fca68e8a50e0b8d4bd4f96b2fc89c98
muhamed <mdrinjak1@etf.unsa.ba>
2021-08-27 23:41:38 +0200

    dodana lista parkinga i popravljena navigacija

--- src/main/java/ba/unsa/etf/icr/projekat/Main.java
+++ src/main/java/ba/unsa/etf/icr/projekat/Main.java
@@ -5,7 +5,10 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
@@ -15,6 +18,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         primaryStage.setTitle("Prijava");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {
+            if (KeyCode.ESCAPE == event.getCode()) {
+                primaryStage.close();
+            }
+        });
         //Platform.runLater( () -> root.requestFocus() );
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
